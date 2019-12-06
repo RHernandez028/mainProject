f019402c47a68e8ef6b24db2bde12088a7a7d26d
Gabriel <rhernadnez028@west-mec.org>
2019-12-05 18:22:17 -0700

    got data up an running on rnm page

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const ejs = require('ejs');
+const path = require('path')
 const app = express();
 const PORT= 8080;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+    res.send('Home Page is Open');
+});
+
+app.get('/rnm/:id',  async (req, res) => {
     
+    try{
+        rickData = await fetch(`https://rickandmortyapi.com/api/character/${req.params.id}`);
+        const json = await rickData.json();
+        res.render('layouts/rick', {
+            data: {
+                name: json.name
+            }            
+        })
+    } catch (error){
+        console.log(error)
+    }
 
-// })
 
-app.listen(PORT, ()=> console.log(`page is open on ${PORT}`))
+});
+
+app.listen(PORT)
