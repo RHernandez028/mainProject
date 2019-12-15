635eef2ea05289cf21ef452692e060c1e2cc8816
Raymond Hernandez <rhernandez028@west-mec.org>
2019-12-15 01:08:48 -0700

    little tings

--- app.js
+++ app.js
@@ -1,102 +1,95 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const ejs = require('ejs');
-const path = require('path')
+const express = require("express");
+const fetch = require("node-fetch");
+const ejs = require("ejs");
+const path = require("path");
 const app = express();
-const PORT= 8080;
+const PORT = 8080;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use( express.static( "public" ) );
-
-app.get('/', (req, res) => {
-
-    res.render('index',{
-        test: "charmander"
-    });
+app.get("/", (req, res) => {
+  res.render("index", {
+    test: "charmander"
+  });
 });
-
 
 // Rick and Morty Pages
-app.get('/rnm', async (req, res) => {
- 	try {
-        rnmCharacters = await fetch(`https://rickandmortyapi.com/api/character/`);
-        // .then(res=>res.json());
-        const json = await rnmCharacters.json();
-        // console.log(json);
-        const [...characters] = json.results;
+app.get("/rnm", async (req, res) => {
+  try {
+    rnmCharacters = await fetch(`https://rickandmortyapi.com/api/character/`);
+    // .then(res=>res.json());
+    const json = await rnmCharacters.json();
+    // console.log(json);
+    const [...characters] = json.results;
 
+    // console.log(characters);
 
-        // console.log(characters);
-
-        res.render('rick', {
-            characters: characters           
-        })
-
-         
-     } catch (error) {
-         console.log(error);
-     }
+    res.render("rick", {
+      characters: characters
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get('/rnm/:id', async (req, res) => {
-
-    try{
-        rickData = await fetch(`https://rickandmortyapi.com/api/character/${req.params.id}`);
-        const json = await rickData.json();
-        res.render('rnmChar', {
-            data: {
-                name: json.name,
-                image: json.image,
-                status: json.status,
-                gender: json.gender,
-                originalDimension: json.origin.name
-            }            
-        })
-    } catch (error){
-        console.log(error)
-    }
-
+app.get("/rnm/:id", async (req, res) => {
+  try {
+    rickData = await fetch(
+      `https://rickandmortyapi.com/api/character/${req.params.id}`
+    );
+    const json = await rickData.json();
+    res.render("rnmChar", {
+      data: {
+        name: json.name,
+        image: json.image,
+        status: json.status,
+        gender: json.gender,
+        originalDimension: json.origin.name
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
-
 
 // Breaking Bad Pages
-app.get('/bb', async (req, res) => {
-    try {
-       bbChar = await fetch(`https://www.breakingbadapi.com/api/characters/`);
-       // .then(res=>res.json());
-       const json = await bbChar.json();
-       const [...characters] = json;
+app.get("/bb", async (req, res) => {
+  try {
+    bbChar = await fetch(`https://www.breakingbadapi.com/api/characters/`);
+    // .then(res=>res.json());
+    const json = await bbChar.json();
+    const [...characters] = json;
 
-       res.render('bbChar', {
-           characters: characters
-       })  
-    } catch (error){
-        console.log(error);
-    }
+    res.render("bbChar", {
+      characters: characters
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get('/bb/:id', async (req, res) => {
-    try{
-        bbData = await fetch(`https://www.breakingbadapi.com/api/characters/${req.params.id}`);
-        const json = await bbData.json();
-        console.log(json);
+app.get("/bb/:id", async (req, res) => {
+  try {
+    bbData = await fetch(
+      `https://www.breakingbadapi.com/api/characters/${req.params.id}`
+    );
+    const json = await bbData.json();
+    // console.log(json);
 
-        res.render('bbCharCards', {
-            data: {
-                name: json[0].name,
-                image: json[0].img
-            }           
-        })
-
-        
-    } catch (error){
-        console.log(error)
-    }
+    res.render("bbCharCards", {
+      data: {
+        name: json[0].name,
+        image: json[0].img,
+        nickname: json[0].nickname,
+        actor: json[0].portrayed,
+        occupation: json[0].occupation
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-
-
 
 app.listen(PORT);
